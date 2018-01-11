cff868666c366ad91e64fce4aef7e71a6cf8f23b
Shlok Amin <samin@naalewis.corporate.thoughtworks.com>
2018-01-11 16:51:30 -0600

    emily + shlok, added story 2

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
 
+    private ArrayList<String> allBooks;
+
     public void start() {
-        System.out.print("Welcome");
+        System.out.println("Welcome");
+        printBooks();
+    }
+
+    public ArrayList<String> getBooks() {
+        return allBooks;
+    }
+
+    public Biblioteca() {
+        allBooks = new ArrayList<String>();
+    }
+
+    public Biblioteca(ArrayList<String> allBooks) {
+        this.allBooks = allBooks;
+    }
+
+    public void printBooks() {
+
+        if (allBooks.isEmpty()){
+            return;
+        }
+
+        String books = "";
+
+        for (String book: allBooks) {
+            books += book + " ";
+        }
+
+        System.out.println(books.substring(0, books.length()-1));
+
     }
 }
--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -3,26 +3,57 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaTest {
 
-    @Test
-    public void shouldReturnWelcomeWhenBibliotecaStarts() {
+
+    public void printSetup(OutputStream os) {
         // Arrange our objects
-        OutputStream os = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(os);
         System.setOut(ps);
+    }
 
-
+    @Test
+    public void shouldPrintWelcomeWhenNoBooks() {
+        OutputStream os = new ByteArrayOutputStream();
+        printSetup(os);
         Biblioteca biblioteca = new Biblioteca();
 
         // Action we are testing
         biblioteca.start();
-        
+
+        String expected = "Welcome\n";
+
         // Assert that the action caused the expected result
-        assertEquals("Welcome",os.toString());
+        assertEquals(expected,os.toString());
+
+        }
+
+     @Test
+    public void shouldPrintWelcomeAndOneBook() {
+         OutputStream os = new ByteArrayOutputStream();
+         printSetup(os);
+         ArrayList<String> allBooks = new ArrayList<>();
+
+         allBooks.add("The Hobbit");
+
+         Biblioteca biblioteca = new Biblioteca(allBooks);
+
+         // Action we are testing
+         biblioteca.start();
+
+         String expected = "Welcome\nThe Hobbit\n";
+
+         // Assert that the action caused the expected result
+         assertEquals(expected,os.toString());
+     }
+
+
+
     }
-}
+
 
